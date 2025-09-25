eaa9dccfc0a645c678ace6797b65f4c99baaa5ee
NolanMastro <nolandmastro@gmail.com>
2025-09-25 13:56:20 -0700

    Clean commit without token

--- src/App.java
+++ src/App.java
@@ -1,5 +1,32 @@
-public class App {
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+
+public class App extends ListenerAdapter{
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String token = System.getenv("discord_bot_tokenn");
+        if (token == null){
+            throw new IllegalStateException("Discord token not found in environmental variables.");
+        }
+
+        JDABuilder.createDefault(token).enableIntents(GatewayIntent.MESSAGE_CONTENT) .addEventListeners(new App()).build();
+    }
+
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event){
+        if (event.getAuthor().isBot()){
+            return;
+        }
+
+        String message = event.getMessage().getContentRaw();
+
+        if (message.equalsIgnoreCase("!ping")){
+            event.getChannel().sendMessage("Pong!").queue();
+        }
     }
 }
