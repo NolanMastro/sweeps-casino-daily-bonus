286f6027f1d1bb86db020d898c17b7418801de87
Nolan <nolandmastro@gmail.com>
2025-09-24 19:15:17 -0700

    Setup base discord bot, add tester method

--- src/App.java
+++ src/App.java
@@ -1,5 +1,28 @@
-public class App {
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+
+public class App extends ListenerAdapter{
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String token = "token";
+
+        JDABuilder.createDefault(token).addEventListeners(new App()).build();
+    }
+
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event){
+        if (event.getAuthor().isBot()){
+            return;
+        }
+
+        String message = event.getMessage().getContentRaw();
+
+        if (message.equalsIgnoreCase("!ping")){
+            event.getChannel().sendMessage("Pong!").queue();
+        }
     }
 }
