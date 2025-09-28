3456b2c892b09bcfbbac2350db435b06660527a9
NolanMastro <nolandmastro@gmail.com>
2025-09-27 17:20:57 -0700

    fixed basic logic for onMessageReceived

--- src/App.java
+++ src/App.java
@@ -8,8 +8,10 @@ import java.io.InputStreamReader;
 
 
 public class App extends ListenerAdapter{
+
+    private static final String[] commands = {"run", "help"};
     public static void main(String[] args) throws Exception {
-        String[] commands = {"run", "help"};
+        
         String token = "token";
         if (token == null){
             throw new IllegalStateException("Discord token not found in environmental variables.");
@@ -25,24 +27,29 @@ public class App extends ListenerAdapter{
         }
 
         String message = event.getMessage().getContentRaw();
-        //chumba
-        if (message.toLowerCase().contains("!run")){
-            if (message.substring(5).toLowerCase().contains("chumba")){
-                String[] parts = message.split("\\s+", 4);
-                if (parts.length < 4) {
-                    event.getChannel().sendMessage("Usage: `!run <service> <email> <password>`").queue();
-                    return;
-                }
+        
+        if (checkformat(message, commands)){
+            String[] parts = message.split("\\s+", 4);
+            String command = parts[1].toLowerCase();
 
+            //chumba
+            if (command.equals("run")){
                 String service = parts[1].toLowerCase();
                 String email = parts[2].trim();
                 String password = parts[3].trim();
-                
-                System.out.printf("Running %s with account %s:%s", service,email,password);
 
+                System.out.printf("Running #s for %s:%s", service, email, password);
             }
-            //event.getChannel().sendMessage("Pong!").queue();
+
+            if (command.equals("help")){ //TODO add embed help message.
+                event.getChannel().sendMessage("Usage: `!run <service> <email> <password>`").queue();
+            }
+        }else{
+            event.getChannel().sendMessage("Invalid command format. Try `!help`.").queue();
         }
+
+
+
     }
     public static boolean checkFormat(String message, String[] commands) {
         if (message == null || message.trim().isEmpty()) {
